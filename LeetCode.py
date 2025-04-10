aed714e6ee632792a5ca6518fc1a5ae38bb49162
idunkoverpuppies <atahanayhan09@gmail.com>
2025-04-10 21:23:41 +0200

    Update LeetCode.py

--- LeetCode.py
+++ LeetCode.py
@@ -1,2 +1,13 @@
+s = "abcabcbb"
 
+def lengthOfLongestSubstring(x):
+    output = 0
+    s_list = []
+    s_dict = {}
+    for i in range(len(x)):
+        s_dict.update(x[i])
+        if x[i] != x[i-1]:
+            output += 1
+    print(output)
+    print(s_dict)
 
